39f82d8608710198a92b10c9bd085fb6d74d3d6b
Amr109 <grand.109.master@gmail.com>
2022-08-23 08:24:40 +0300

    sidebar second

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,6 +1,13 @@
 <script>
 export default {
   name: 'side-bar',
+  methods: {
+    async toggelFocesed(arr) {
+      arr.map((e) => {
+        e.focused = false
+      })
+    },
+  },
   data() {
     return {
       items: [
@@ -10,18 +17,22 @@ export default {
   <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
   <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 </svg>`,
+          focused: true,
         },
+
         {
           title: 'Dashboard',
           icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-grid-1x2" viewBox="0 0 16 16">
   <path d="M6 1H1v14h5V1zm9 0h-5v5h5V1zm0 9v5h-5v-5h5zM0 1a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm9 0a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V1zm1 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1h-5z"/>
 </svg>`,
+          focused: false,
         },
         {
           title: 'Analytics',
           icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pie-chart" viewBox="0 0 16 16">
   <path d="M7.5 1.018a7 7 0 0 0-4.79 11.566L7.5 7.793V1.018zm1 0V7.5h6.482A7.001 7.001 0 0 0 8.5 1.018zM14.982 8.5H8.207l-4.79 4.79A7 7 0 0 0 14.982 8.5zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
 </svg>`,
+          focused: false,
         },
         {
           title: 'Schedules',
@@ -29,6 +40,7 @@ export default {
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
   <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
 </svg>`,
+          focused: false,
         },
         {
           title: 'History',
@@ -36,6 +48,7 @@ export default {
   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
 </svg>`,
+          focused: false,
         },
       ],
     }
@@ -44,23 +57,37 @@ export default {
 </script>
 
 <template>
-  <div class="col-span-2 bg-white">
-    <div class="p-[20px]">
-      <a href="#" class="my-[20px] flex items-center">
-        <img src="./../../public/favicon.ico" class="h-8 w-8 grayscale-[100%] filter" />
-        <h1 class="ml-2 text-[24px]">Pure Feed</h1>
-      </a>
+  <div class="col-span-2 grid grid-rows-6 bg-white">
+    <div class="">
+      <div class="p-[20px]">
+        <a href="#" class="my-[20px] flex items-center">
+          <img src="./../../public/favicon.ico" class="h-8 w-8" />
+          <h1 class="ml-2 text-[24px]">Pure Feed</h1>
+        </a>
+      </div>
       <div>
-        <ul>
-          <li v-for="item in items" :key="item" class="my-[12px] flex h-[48px] items-center">
-            <a href="#" class="flex items-center">
-              <div class="flex h-[24px] w-[24px] items-center" v-html="item.icon"></div>
-              <p class="ml-[16px] text-[16px]">{{ item.title }}</p>
+        <ul class="space-y-2">
+          <li
+            v-for="item in items"
+            :key="item"
+            class="flex h-[48px] items-center"
+            @click="toggelFocesed(items), (item.focused = true)"
+          >
+            <a
+              href="#"
+              class="my-[12px] w-full p-[20px]"
+              :class="item.focused ? ' border-l-4 border-green-600 bg-gray-100' : 'ml-[4px]'"
+            >
+              <div class="flex items-center">
+                <div class="flex h-[24px] w-[24px] items-center" v-html="item.icon"></div>
+                <p class="ml-[16px] text-[16px]">{{ item.title }}</p>
+              </div>
             </a>
           </li>
         </ul>
       </div>
     </div>
+    <div class="row-span-1"></div>
   </div>
 </template>
 
